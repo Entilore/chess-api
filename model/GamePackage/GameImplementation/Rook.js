2aef66730b9c6cb558e43c0841d3bf4b206f92df
Thomas Hareau <thomashareau@free.fr>
2017-05-31 01:07:50 +0200

    improve cells acquisition

--- model/GamePackage/GameImplementation/Rook.js
+++ model/GamePackage/GameImplementation/Rook.js
@@ -24,15 +24,9 @@ export class Rook extends Piece {
 		super.getAccessibleCells(x, y);
 		let cells = [];
 
-		for (let f in [
-			(i, j) => [i + 1, j],
-			(i, j) => [i - 1, j],
-			(i, j) => [i, j + 1],
-			(i, j) => [i, j - 1]]
-			) {
-			for (let [i, j] of Piece._computeCells(x, y, f)) {
-				if (i !== x && j !== y) cells.push([i, j]);
-			}
+		for (let i = 0; i < 8; i ++){
+			cells.push([i,y]);
+			cells.push([x,i]);
 		}
 
 		this.pathes[[x, y]] = cells;
